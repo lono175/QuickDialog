f5bd86508e6ef3dcb5f3843f5d2547faf52716a3
Lono <khaifeng@gmail.com>
2012-12-04 19:16:46 +0800

    - add title label for QSegmentedElement

--- quickdialog/QSegmentedElement.m
+++ quickdialog/QSegmentedElement.m
@@ -46,47 +46,33 @@
 
     [self handleElementSelected:_controller];
 }
-
-/*
- 
-- (UITableViewCell *)getCellForTableView:(QuickDialogTableView *)tableView controller:(QuickDialogController *)controller {
-    _controller = controller;
-    QTableViewCell *cell = [[QTableViewCell alloc] init];
-    cell.backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
-    cell.backgroundColor = [UIColor clearColor];
-    UISegmentedControl *control = [[UISegmentedControl alloc] initWithItems:_items];
-    [control addTarget:self action:@selector(handleSegmentedControlValueChanged:) forControlEvents:UIControlEventValueChanged];
-    const BOOL isPhone = [UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPhone;
-    control.frame = CGRectMake(isPhone ? 9 : 30, 0, isPhone ? 302 : 260, self.height);
-    control.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    control.segmentedControlStyle = UISegmentedControlStyleBar;
-    control.selectedSegmentIndex = _selected;
-    control.tag = 4321;
     
-    [cell addSubview:control];
-    return cell;
-}
-
-*/
-
 // heavily modified to clip the round borders of the segmented control with extra elements of defined size
 - (UITableViewCell *)getCellForTableView:(QuickDialogTableView *)tableView controller:(QuickDialogController *)controller {
     _controller = controller;
     QTableViewCell *cell = [[QTableViewCell alloc] init];
-    cell.backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
-    cell.backgroundColor = [UIColor clearColor];
+    //cell.backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
+    //cell.backgroundColor = [UIColor clearColor];
+    cell.textLabel.text = _title;
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
     
     NSMutableArray * item = [NSMutableArray arrayWithArray:_items];
     [item addObject:@""];
     [item insertObject:@"" atIndex:0];
+    
     CGRect frame = cell.contentView.bounds;
     frame.origin.x -=1;
-    frame.size.width += 2;
-    frame.size.height += 1;    
+    frame.size.width -= 35;
+    frame.size.height -= 15;
+    
+    //const BOOL isPhone = [UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPhone;
+    //CGRect frame = CGRectMake(isPhone ? 9 : 30, 7.5, isPhone ? 302 : 260, 30.0);
+
     UIView * container = [[UIView alloc] initWithFrame: frame];
-    container.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    //container.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    container.autoresizingMask = UIViewAutoresizingNone;
     container.clipsToBounds = YES;
-    [cell.contentView addSubview:container];
+    //[cell.contentView addSubview:container];
     
     UISegmentedControl *control = [[UISegmentedControl alloc] initWithItems:item];
     [control addTarget:self action:@selector(handleSegmentedControlValueChanged:) forControlEvents:UIControlEventValueChanged];
@@ -102,7 +88,9 @@
     [control setWidth:10 forSegmentAtIndex:0];
     [control setWidth:10 forSegmentAtIndex:item.count-1];
 
+    
     [container addSubview:control];
+    cell.accessoryView = container;
     return cell;
 }
 
